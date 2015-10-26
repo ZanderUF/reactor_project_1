ba4768c2a8ab54df7e8803115713387807f3181e
Zander Mausolff <zanderm@mseadminsimac2.ad.ufl.edu>
2015-10-25 23:30:24 -0400

    close

--- project1.py
+++ project1.py
@@ -10,66 +10,87 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+
 ####------------Variables----------------###
-num_src_neut = 10000
+num_src_neut = input('Enter the number of source neutrons: ')
 y=0
-density = 2.3*(1 + 0.1*y)
-density_oil = .875
-intial_energy = 2.5 #Units: MeV
+#density = 2.3*(1 + 0.1*y) #Units: g/cm^3
+density_oil = .875 #Units g/cm^3
 Avagadro = 6.0022e23
 ##Random number seeds##
-np.random.seed(0)
 np.random.seed(1)
 
-oil_dist = 10
-oil_depth = 5#Units: meters
-y_max = 25
-a=2.3
-b=0.1
+oil_dist = 20 #distance the oil is from the groud Units: cm
+oil_depth = 5#Units: cm
+y_max = 25 #Units: cm
+a=2.3 # parameters for variable density
+b=0.1 # parameters for variable density
 ###-------Cross Sections----------------###
-##thermal##
+###Units: Barns (10^-24 cm)
+##--------thermal-----------------------###
 calcium_xs_total_thermal = 3.45901
 calcium_xs_elastic_thermal = 3.01605
-carbon_xs_total_thermal= 4.94262
-carbon_xs_elastic_thermal = 4.93925
-oxygen_xs_total_thermal = 3.96192
-oxygen_xs_elastic_thermal = 3.96173
-hydrogen_xs_total_thermal = 30.4007
-hydrogen_xs_elastic_thermal = 30.0687
+carbon_xs_total_thermal= 4.94262 * 12
+carbon_xs_elastic_thermal = 4.93925 * 12
+oxygen_xs_total_thermal = 3.96192 * 3 
+oxygen_xs_elastic_thermal = 3.96173 * 3
+hydrogen_xs_total_thermal = 30.4007 * 5
+hydrogen_xs_elastic_thermal = 30.0687 * 5
 sulfur_xs_total_thermal = 1.52499
 sulfur_xs_elastic_thermal = 0.995363
-nitro_xs_total_thermal = 12.3647
-nitro_xs_elastic_thermal = 10.3596
-##fast##
+#nitro_xs_total_thermal = 12.3647
+#nitro_xs_elastic_thermal = 10.3596
+##--------fast-----------------------###
 calcium_xs_total_fast = 2.24281
 calcium_xs_elastic_fast = 0.874423
-carbon_xs_total_fast = 2.38502
-carbon_xs_elastic_fast = 2.37137
-oxygen_xs_total_fast = 2.79387
-oxygen_xs_elastic_fast = 2.78041
-hydrogen_xs_total_fast = 3.98955
-hydrogen_xs_elastic_fast = 3.98952
+carbon_xs_total_fast = 2.38502 * 12
+carbon_xs_elastic_fast = 2.37137 * 12
+oxygen_xs_total_fast = 2.79387 * 3
+oxygen_xs_elastic_fast = 2.78041 * 3
+hydrogen_xs_total_fast = 3.98955 * 5
+hydrogen_xs_elastic_fast = 3.98952 * 5
 sulfur_xs_total_fast = 2.67586
 sulfur_xs_elastic_fast = 2.38327
-nitro_xs_total_fast = 1.9781
-nitro_xs_elastic_fast = 1.84764
+#nitro_xs_total_fast = 1.9781
+#nitro_xs_elastic_fast = 1.84764
 
-##total cross sections
+##--------total cross sections ------###
 scatter_xs_lime_thermal = calcium_xs_elastic_thermal + carbon_xs_elastic_thermal + oxygen_xs_elastic_thermal
 scatter_xs_lime_fast = calcium_xs_elastic_fast + carbon_xs_elastic_fast + oxygen_xs_elastic_fast
 
-scatter_xs_oil_thermal = carbon_xs_elastic_thermal + hydrogen_xs_elastic_thermal + nitro_xs_elastic_thermal + sulfur_xs_total_thermal
-scatter_xs_oil_fast = carbon_xs_elastic_fast + hydrogen_xs_elastic_fast + nitro_xs_elastic_fast + sulfur_xs_total_fast
+scatter_xs_oil_thermal = carbon_xs_elastic_thermal + hydrogen_xs_elastic_thermal  + sulfur_xs_total_thermal
+scatter_xs_oil_fast = carbon_xs_elastic_fast + hydrogen_xs_elastic_fast  + sulfur_xs_total_fast
 
 total_xs_lime_thermal = calcium_xs_total_thermal + carbon_xs_total_thermal + oxygen_xs_total_thermal 
 total_xs_lime_fast = calcium_xs_total_fast + carbon_xs_total_fast + oxygen_xs_total_fast
 abs_xs_lime_thermal = (calcium_xs_total_thermal - calcium_xs_elastic_thermal) + (carbon_xs_total_thermal-carbon_xs_elastic_thermal) + (oxygen_xs_total_thermal -oxygen_xs_elastic_thermal)
 abs_xs_lime_fast = (calcium_xs_total_fast - calcium_xs_elastic_fast) + (carbon_xs_total_fast - carbon_xs_elastic_fast) + (oxygen_xs_total_fast-oxygen_xs_elastic_fast)
 
-total_xs_oil_thermal = carbon_xs_total_thermal + hydrogen_xs_total_thermal + nitro_xs_total_thermal + sulfur_xs_total_thermal
-total_xs_oil_fast = carbon_xs_total_fast + hydrogen_xs_total_fast + nitro_xs_total_fast + sulfur_xs_total_fast
-abs_xs_oil_thermal = (carbon_xs_total_thermal-carbon_xs_elastic_thermal) + (hydrogen_xs_total_thermal - hydrogen_xs_elastic_thermal) + (nitro_xs_total_thermal - nitro_xs_elastic_thermal) + (sulfur_xs_total_thermal- sulfur_xs_elastic_thermal)
-abs_xs_oil_fast = (carbon_xs_total_fast - carbon_xs_elastic_fast) + (hydrogen_xs_total_fast - hydrogen_xs_elastic_fast) + (nitro_xs_total_fast - nitro_xs_total_fast) + (sulfur_xs_total_fast - sulfur_xs_elastic_fast)
+total_xs_oil_thermal = carbon_xs_total_thermal + hydrogen_xs_total_thermal + sulfur_xs_total_thermal
+total_xs_oil_fast = carbon_xs_total_fast + hydrogen_xs_total_fast + sulfur_xs_total_fast
+abs_xs_oil_thermal = (carbon_xs_total_thermal-carbon_xs_elastic_thermal) + (hydrogen_xs_total_thermal - hydrogen_xs_elastic_thermal) + (sulfur_xs_total_thermal- sulfur_xs_elastic_thermal)
+abs_xs_oil_fast = (carbon_xs_total_fast - carbon_xs_elastic_fast) + (hydrogen_xs_total_fast - hydrogen_xs_elastic_fast)  + (sulfur_xs_total_fast - sulfur_xs_elastic_fast)
+
+###-----------From PNNL---------------###
+### Oil, crude (Mexican)
+#Element         weight          Atom        Atom
+#                fraction        fraction    density
+#H 1001  1000    0.104039        0.587752    0.060606
+#C 6000  6000    0.853733        0.404749    0.041736
+#S 16000 16000   0.042228        0.007499    0.000773
+H_frac = 0.587752
+C_frac = 0.404749
+S_frac = 0.007499
+
+#                             Weight   Atom     Atom
+#Element Neutron              Fraction Fraction Density
+#C 6000 6000                  0.120003 0.200002 0.016847
+#O 8016 8000                  0.479554 0.599991 0.050541
+#Ca 20000 20000               0.400443 0.200007 0.016848
+#Total                        1.000000 1.000000 0.084236
+C_lime_frac = 0.200002
+O_frac = 0.599991
+Ca_frac = 0.200007
 
 ##Avg Number of collions to thermalize##
 calcium_collision = 375.2984654	
@@ -77,12 +98,11 @@ carbon_collision = 116.8557575
 oxygen_collision = 153.5684244
 hydrogen_collision = 18.42386871
 sulfur_collision = 301.502774	
-nitro_collision = 135.2254046
+#nitro_collision = 135.2254046
 
-limestone_collision = (calcium_collision + carbon_collision + oxygen_collision)/3
-oil_collision = (carbon_collision + hydrogen_collision + nitro_collision + sulfur_collision)/4
-print 'limestone collisions', limestone_collision
-print 'oil collision', oil_collision
+limestone_collision = (calcium_collision*Ca_frac + carbon_collision*C_lime_frac + oxygen_collision*O_frac*3)
+oil_collision = (carbon_collision*C_frac + hydrogen_collision*H_frac + sulfur_collision*S_frac)
+
 
 ##Mass Numbers##
 calcium_mass	= 40.078	
@@ -90,17 +110,16 @@ carbon_mass	= 12.0107
 oxygen_mass	= 15.9994
 hydrogen_mass   = 1.00794
 sulfur_mass	= 32.065
-nitro_mass   = 14.0067
+#nitro_mass   = 14.0067
 
 limestone_mass = 100.0869 #Units: g/mol [src: PNNL]
-oil_mass = 200  #Units: g/mol [src: PNNL]			
+oil_mass = 250  #Units: g/mol [src: PNNL]			
 
 ##arrays for plotting
 dist_array = []
 abs_array = []
 leaked_array = []
 scat_array = []
-interaction_ar = []
 ###--------------------------------------###
 tally={'absorbed':0,'leaked':0,'transmitted':0}
 tally_group = {'fast':0,'thermal':0}
@@ -155,35 +174,28 @@ class Neutron(object):
             density_max = density_oil
         ## Find max macroscopic xs
 
-        previous_pt = self.distance
-        flag='False'
-        while flag == 'False':
-            if self.material == 'limestone':
-                density_max = calc_density(a,b,y_max)
-            ## Find max macroscopic xs ##
-            macro_xs_max = calc_macro_xs(Avagadro,self.mass,density_max,self.sigma_s)
-            ##--get new path length--##
-            path_length = -1/macro_xs_max * np.log(np.random.rand(1))
-            ##Sample new angle
-            self.direction = np.random.uniform(-1,1)
-            ##--Calculate new point--##
-            new_pt = previous_pt + path_length*self.direction
-            ##--Find second random number to determine if path length should be accepted##     
-            den_at_pt = calc_density(a,b,new_pt)
-            xs_at_pt = calc_macro_xs(Avagadro,self.mass,den_at_pt,self.sigma_s)
-            rand_2 = np.random.rand(1)
-            if rand_2 < np.divide(xs_at_pt,macro_xs_max):
-                ##pt is accpeted
-                flag = 'True'
-                self.distance = previous_pt + new_pt 
-  
+        macro_xs_max = calc_macro_xs(Avagadro,self.mass,density_max,self.sigma_s)
+        ##--get new path length--##
+        path_length = np.divide(-1,macro_xs_max) * np.log(np.random.rand(1))
+        ##Sample new angle
+        self.direction = np.random.uniform(-1,1)
+        ##--Calculate new point--##
+        new_pt = previous_pt + path_length*self.direction
+        ##--Find second random number to determine if path length should be accepted##     
+        rand_2 = np.random.rand()
+        den_at_pt = calc_density(a,b,new_pt)
+        xs_at_pt = calc_macro_xs(Avagadro,self.mass,den_at_pt,self.sigma_s)
+        if rand_2 < np.divide(xs_at_pt,macro_xs_max):
+            ##pt is accpeted
+            self.distance = previous_pt + new_pt  
+        scat_array.append(self.distance)
 ####-------in limestone------####
     def in_lime_stone(self):
         abs_rand = np.random.rand(1)
         self.mass = limestone_mass
         self.collisions = limestone_collision
         self.material = 'limestone'
-
+        self.down_scatter()
         #check group
         if self.group == 'fast':
             xs_comp = abs_xs_lime_fast/total_xs_lime_fast
@@ -196,23 +208,18 @@ class Neutron(object):
             self.absorbed()
         else:
             #scattering
-            self.down_scatter()
             self.scatter()
-            if self.distance > 25 :
-                #neutron reflected past point of interest           
-                self.transmitted()
-                interaction_ar.append(self.distance)
-     
             if self.distance < 0 :
                 #neutron reflected back to surface
                 self.leaked()
-                interaction_ar.append(self.distance)
-
             elif oil_dist < self.distance < oil_dist +5:
                 #neutron in oil
                 self.in_oil()
             else:
                 self.in_lime_stone()
+            if self.distance > 25 :
+                #neutron reflected past point of interest           
+                self.transmitted()
 
 ###--------in Oil---------###
     def in_oil(self):
@@ -220,7 +227,7 @@ class Neutron(object):
         self.mass = oil_mass
         self.collisions = oil_collision
         self.material = 'oil'
-
+        self.down_scatter()
         #check group
         if self.group == 'fast':
             xs_comp = abs_xs_oil_fast /total_xs_oil_fast
@@ -233,7 +240,6 @@ class Neutron(object):
             self.absorbed()
         else:
             #scattering
-            self.down_scatter()
             self.scatter()
             if self.distance < 0 :
                 #neutron reflected back to surface
@@ -244,10 +250,7 @@ class Neutron(object):
                 #neutron reflected past point of interest
                 self.in_lime_stone
             if self.distance > oil_dist+oil_depth :
-                if self.distance > 25:
-                    self.transmitted()
-                else:
-                    self.in_lime_stone()
+                self.in_lime_stone()
             elif self.distance > 25:
                 self.transmitted()
                     
@@ -256,10 +259,7 @@ class Neutron(object):
         #determine if in oil or limestone
         if self.distance < oil_dist or self.distance > oil_dist + oil_depth:  
             #in limestone
-            if self.distance > y_max:
-                self.transmitted()
-            else:
-                self.in_lime_stone()
+            self.in_lime_stone()
         else:
             #in oil
             self.in_oil()
